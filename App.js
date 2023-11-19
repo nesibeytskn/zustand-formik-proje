499cc3d4a656f103992a9aeb1122c693235820aa
nesibeytskn <nesibeyetiskin1@gmail.com>
2023-11-19 18:39:54 +0300

    react hook form ile validation oluşturuldu

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import useCounterStore from './src/app/store';
 import {Formik} from 'formik';
 import Counter from './src/components/Counter';
 import * as Yup from 'yup';
+import {useForm, Controller} from 'react-hook-form';
 
 const validationScheme = Yup.object().shape({
   email: Yup.string()
@@ -23,10 +24,20 @@ const validationScheme = Yup.object().shape({
 
 const App = () => {
   const showZuatandInfo = false;
+  const isShowFormik = false;
+  const isShowReactHookForms = true;
   const {increment, decrement} = useCounterStore(state => state);
-  const handleOnSubmit = values => {
-    console.log('handleSubmit', values);
+  const {
+    control,
+    handleSubmit,
+    formState: {errors, isValid},
+    getValues,
+  } = useForm({defaultValues: {email: '', password: ''}, mode: 'onChange'});
+  const handleOnSubmit = () => {
+    console.log('handleSubmit', handleOnSubmit);
   };
+
+  console.log('err', errors);
   return (
     <SafeAreaView style={{marginHorizontal: 10}}>
       {showZuatandInfo && (
@@ -36,39 +47,105 @@ const App = () => {
           <Button title="Azalt" onPress={decrement} />
         </View>
       )}
-      <View>
-        <Formik
-          validationSchema={validationScheme}
-          initialValues={{email: '', password: ''}}
-          onSubmit={handleOnSubmit}>
-          {({handleChange, handleSubmit, values, errors, touched, isValid}) => (
-            <View style={styles.inputContainer}>
-              <Text>E-Posta</Text>
-              <TextInput
-                placeholder="E-Posta"
-                style={styles.input}
-                onChangeText={handleChange('email')}
-                value={values.email}
-                keyboardType="email-address"
-                autoCapitalize={'none'}
-              />
-              <Text style={styles.errorText}>{errors.email}</Text>
-              <Text>Password</Text>
-              <TextInput
-                placeholder="Password"
-                style={styles.input}
-                onChangeText={handleChange('password')}
-                secureTextEntry
-                value={values.password}
-                autoCapitalize={'none'}
-              />
-              <Text style={styles.errorText}>{errors.password}</Text>
+      {isShowFormik && (
+        <View>
+          <Formik
+            validationSchema={validationScheme}
+            initialValues={{email: '', password: ''}}
+            onSubmit={handleOnSubmit}>
+            {({
+              handleChange,
+              handleSubmit,
+              values,
+              errors,
+              touched,
+              isValid,
+              dirty,
+            }) => (
+              <View style={styles.inputContainer}>
+                <Text>E-Posta</Text>
+                <TextInput
+                  placeholder="E-Posta"
+                  style={styles.input}
+                  onChangeText={handleChange('email')}
+                  value={values.email}
+                  keyboardType="email-address"
+                  autoCapitalize={'none'}
+                />
+                <Text style={styles.errorText}>{errors.email}</Text>
+                <Text>Password</Text>
+                <TextInput
+                  placeholder="Password"
+                  style={styles.input}
+                  onChangeText={handleChange('password')}
+                  secureTextEntry
+                  value={values.password}
+                  autoCapitalize={'none'}
+                />
+                <Text style={styles.errorText}>{errors.password}</Text>
 
-              <Button title="Giriş Yap" onPress={handleSubmit} disabled={!isValid} />
-            </View>
-          )}
-        </Formik>
-      </View>
+                <Button
+                  title="Giriş Yap"
+                  onPress={handleSubmit}
+                  disabled={!isValid || dirty}
+                />
+              </View>
+            )}
+          </Formik>
+        </View>
+      )}
+      {isShowReactHookForms && (
+        <View>
+          <View style={styles.inputContainer}>
+            <Text>E-Posta</Text>
+            <Controller
+              control={control}
+              name="email"
+              rules={{required: {value: true, message: 'Bu alan zorunludur'}}}
+              render={({field: {onChange, onBlur, value, errors}}) => (
+                <TextInput
+                  placeholder="E-Posta"
+                  style={styles.input}
+                  keyboardType="email-address"
+                  autoCapitalize={'none'}
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  errors={errors}
+                />
+              )}
+            />
+
+            {errors.email && <Text>{errors?.email?.message}</Text>}
+            <Text>Password</Text>
+            <Controller
+              control={control}
+              name="password"
+              rules={{
+                required: {value: true, message: 'Bu alan zorunludur'},
+                maxLength: {
+                  value: 20,
+                  message: 'Şifre alanı en çok 20 karakter olmalıdır',
+                },
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  placeholder="Password"
+                  style={styles.input}
+                  secureTextEntry
+                  autoCapitalize={'none'}
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                />
+              )}
+            />
+            {errors.password && <Text>{errors?.password?.message}</Text>}
+
+            <Button title="Giriş Yap" onPress={handleSubmit(handleOnSubmit)} />
+          </View>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
